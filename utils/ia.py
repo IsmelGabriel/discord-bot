fd41833781c160488d64a10fd5d2b62950b8a909
Ismel Salazar <ssismel28@gmail.com>
2025-10-30 13:12:54 -0500

    funciones de openia funcionando - prueba local

--- utils/ia.py
+++ utils/ia.py
@@ -1,18 +1,20 @@
-from openai import OpenAi
+from openai import OpenAI
 import os
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-ia_client = OpenAi(api_key=openai_api_key)
+ia_client = OpenAI(api_key=openai_api_key)
 
 def generate_response(prompt: str) -> str:
     response = ia_client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4.1-mini",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "system", "content": { "Your name is ZioTiki Bot.",
+                                            "You are a helpful assistant in discord with multiple users.",
+                                            "Provide clear and concise answers to user questions."}},
             {"role": "user", "content": prompt}
         ],
         max_tokens=150,
         temperature=0.7
     )
-    return response.choices[0].message['content']
+    return response.choices[0].message.content
